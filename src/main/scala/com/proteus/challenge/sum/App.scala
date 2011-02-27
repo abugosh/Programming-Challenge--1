9f1d51a5144aa368c6447b29db53f6b3e34cc971
Alex Bugosh <abugosh@gmail.com>
2011-02-27 18:08:37 -0500

    A solution at last...

--- src/main/scala/com/proteus/challenge/sum/App.scala
+++ src/main/scala/com/proteus/challenge/sum/App.scala
@@ -9,10 +9,12 @@ object App extends Application {
 }
 
 object Summer {
-  def combinationsFor(num : Int) = {
-    combinate(List(num - 1, 1)).filterNot(_ == Nil)
-  }
 
+  def combinationsFor(num : Int) = {
+    val ret = combinate(List(num - 1, 1)).filterNot(_ == Nil)
+    ret
+  }
+  
   def combinate(sum : List[Int]) : List[List[Int]] = {
     if (sum.head < 1)
       return List(Nil)
@@ -23,7 +25,7 @@ object Summer {
     if (sum.tail == Nil)
       return sum :: combinate(sum.head - 1 :: 1 :: Nil)
 
-    val ret = sum :: (for (y <- (1 to sum.head - 1);
+    var ret = sum :: (for (y <- (1 to sum.head - 1);
                            x = sum.head - y;
                            if (x > sum.tail.head))
                       yield combinate(sum.tail.head + y :: sum.tail.tail).
@@ -31,9 +33,35 @@ object Summer {
                                      else x :: foo)).flatten.toList
     
     if (sum.tail.head == 1)
-      sum.tail.padTo(sum.head + sum.tail.size, 1) :: ret
-    else
-      ret
+      ret = sum.tail.padTo(sum.head + sum.tail.size, 1) :: ret
+
+    ret
   }
+
+  val mem = new scala.collection.mutable.HashMap[Int, Int]
+  mem.put(1, 0)
+  def gen(num : Int) : Int = {
+    if (mem.contains(num))
+      return mem(num)
+    
+    val res = (1 to num - 1).map(x => genLimit(num - x, x)).sum
+
+    mem.put(num, res)
+
+    res
+  }
+
+  def genLimit(limit : Int, amt : Int) : Int = {
+    val res = if (amt <= limit)
+                1 + gen(amt)
+              else
+                gen(amt) - ((1 to (amt - limit - 1)).map(x => genLimit(amt - x, x)).sum)
+
+//    println("limit: " + limit + " amt: " + amt + " res: " + res)
+
+    res
+  }
+
 }
 
+// 190569291
